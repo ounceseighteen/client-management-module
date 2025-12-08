3d11f9abcb876d4085e0c6aecb400e2ca7d874bb
ounceseighteen <ounces.inbox@gmail.com>
2025-12-08 19:46:39 +0400

    Add files via upload

---
+++ client-management-module/src/main/java/com/azimuth/crm/Application.java
@@ -0,0 +1,12 @@
+package com.azimuth.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.println("Client Management Module started!");
+    }
+}
